d8b79a641a568abd227fc7faf05de85a38555038
JayO <bluejayvrstudio@gmail.com>
2025-04-07 19:13:25 -0500

    update

--- IntegrationTests/Integrations.py
+++ IntegrationTests/Integrations.py
@@ -4,7 +4,8 @@ from PyInfiniteTree.Core.TaskStackMachine import StatusMessage
 
 class Integration:
     def __init__(self):
-        self.sigma = StatusMessage.RUNNING 
+        self.sigma = StatusMessage.RUNNING
+        print(f"Initial message is: {self.sigma}")
         print("Initial message is RUNNING: ",  self.sigma == StatusMessage.RUNNING)
         print("Initial message is SUCCESS: ", self.sigma == StatusMessage.SUCCESS)
 
