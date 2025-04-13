f5ab02c92580060d892642bd17505d51a04ed265
JayO <bluejayvrstudio@gmail.com>
2025-04-12 19:07:41 -0500

    update

--- controllers/Controller_InfiniteTree/Controller_InfiniteTree.py
+++ controllers/Controller_InfiniteTree/Controller_InfiniteTree.py
@@ -4,15 +4,11 @@ import numpy as np
 from matplotlib import pyplot as plt
 # import py_trees Behavior Tree library
 import py_trees
-
 # import blackboard singleton
 from blackboard.blackboard import blackboard
 from blackboard.data_tree import DataTree
-
 # import misc behaviors
 from naive_navigation.follow_waypoints import FollowWaypoints
-from naive_navigation.test_behavior import TestBehavior
-
 # webots API
 from controller import Supervisor
 
@@ -21,40 +17,8 @@ robot = Supervisor()
 blackboard.setup(robot)
 
 # SETUP ROOT LEVEL TREES
-dataTree = DataTree("meta tree") # to keep consistent time dependent meta-data such as wheel velocity 
-# tree = RotateClockwise("rotate clockwise", [], 2) # Main behavior tree
-# tree = RotateCounterclockwise("rotate counter-clockwise", [], 2) # Main behavior tree
-# tree = MoveBackwards("move backwards", [], 2, 0.05) # Main behavior tree
-# tree = dRotate("", [], 90, 2)
-# tree = dMove("", [], 0.0254 * 2, 4)
-# tree = MoveToRL("", [], (1.09817, 0.3374))
+dataTree = DataTree("Data tree") # to keep consistent time dependent meta-data such as wheel velocity 
 tree = FollowWaypoints("")
-# tree = TestBehavior("test behavior")
-
-# tree = Selector("Main", children=[
-#     RotateClockwise("rotate clockwise", [], 2), # Main behavior tree
-#     RotateCounterclockwise("rotate counter-clockwise", [], 2), # Main behavior tree
-#     MoveBackwards("move backwards", [], 2, 0.05) # Main behavior tree
-# ], memory = True)
-
-
-# MAIN BEHAVIOR TREE
-# tree = Sequence("Main", children=[
-#     # set arm to a safe position
-#     ResetArm("reset arm to safe position", defaultPoses.default_arm_pos),
-    
-#     # map cspace. Load previously saved map if it exists
-#     Selector("Does map exist?", children=[
-#         DoesMapExist("Test for map"),
-#         Parallel("Mapping", policy=py_trees.common.ParallelPolicy.SuccessOnOne(), children=[
-#             Mapping("map the environment"),
-#             Navigation("move around the table")
-#         ])
-#     ], memory=True),
-
-# ], memory=True)
-
-# tree = FollowWaypoints("")
 
 # Invoke setup on all nodes before stepping through
 dataTree.setup_with_descendants()
@@ -62,10 +26,8 @@ tree.setup_with_descendants()
 
 # step through webots robot and tick behavior tree
 while robot.step(blackboard.timestep) != -1:
-    # tick data tree
-    dataTree.tick_once()
-    # step/tick behavior tree
-    tree.tick_once()
+    dataTree.tick_once() # tick data tree
+    tree.tick_once() # step/tick behavior tree
 
     if tree.status in [py_trees.common.Status.SUCCESS, py_trees.common.Status.FAILURE]:
         print("Tree completed!")
