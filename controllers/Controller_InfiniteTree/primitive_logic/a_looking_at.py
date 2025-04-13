dd6ba905b9baa31544ef030a4dd0ad216ed76b10
BlueJayVRStudio <BlueJayVRStudio@gmail.com>
2025-04-13 14:27:56 -0500

    update

--- controllers/Controller_InfiniteTree/primitive_logic/a_looking_at.py
+++ controllers/Controller_InfiniteTree/primitive_logic/a_looking_at.py
@@ -1,34 +1,18 @@
-import py_trees
+from PyInfiniteTree.Core.TaskStackMachine import Status
+from PyInfiniteTree.Interfaces.Behavior import Behavior
 import numpy as np
-from blackboard.blackboard import blackboard
 from helpers.misc_helpers import *
 
-class a_LookingAt(py_trees.behaviour.Behaviour):
-    def __init__(self, name, WP):
-        super(a_LookingAt, self).__init__(name)
+class a_LookingAt(Behavior):
+    def __init__(self, blackboard, WP):
+        super().__init__(blackboard)
         self.WP = WP
 
-    def setup(self):
-        self.logger.debug("  %s [Foo::setup()]" % self.name)
+    def Step(self, memory, blackboard, message) -> Status:
+        memory.push(self)
+        return self.CheckRequirement()
 
-    def initialise(self):
-        self.logger.debug("  %s [Foo::initialise()]" % self.name)
-    
-    def update(self):
-        self.logger.debug("  %s [Foo::update()]" % self.name)
-
-        if self.CheckRequirement() == py_trees.common.Status.SUCCESS:
-            return py_trees.common.Status.SUCCESS 
-        return py_trees.common.Status.FAILURE
-
-    def CheckRequirement(self):
-        if blackboard.isLookingAt(self.WP, 0.98):
-            return py_trees.common.Status.SUCCESS 
-        return py_trees.common.Status.RUNNING 
-
-    def terminate(self, new_status):
-        self.logger.debug(
-            "  %s [Foo::terminate().terminate()][%s->%s]"
-            % (self.name, self.status, new_status)
-        )
-
+    def CheckRequirement(self) -> Status:
+        if self.blackboard.isLookingAt(self.WP, 0.98):
+            return Status.SUCCESS 
+        return Status.RUNNING 
